a73d5d9ba80b669f7ec1d5e4718d15e33670d078
Hendra Anggrian <hendraanggrian@gmail.com>
2019-10-18 21:41:43 +0700

    Add plugin description

--- locale-gradle-plugin/src/com/hendraanggrian/locale/LocalePlugin.kt
+++ locale-gradle-plugin/src/com/hendraanggrian/locale/LocalePlugin.kt
@@ -12,7 +12,7 @@ import org.gradle.kotlin.dsl.registering
 class LocalePlugin : Plugin<Project> {
 
     companion object {
-        const val GROUP_NAME = "locale"
+        const val GROUP_NAME = "localization"
     }
 
     override fun apply(project: Project) {
@@ -20,6 +20,7 @@ class LocalePlugin : Plugin<Project> {
         project.tasks {
             val localizeJava by registering(LocalizeJavaTask::class) {
                 group = GROUP_NAME
+                description = "Write localization Java Properties file."
                 setTable(ext.javaTable)
                 if (outputDir == null) {
                     outputDir = project.projectDir.resolve("src/main/resources")
@@ -33,6 +34,7 @@ class LocalePlugin : Plugin<Project> {
             }
             val localizeAndroid by registering(LocalizeAndroidTask::class) {
                 group = GROUP_NAME
+                description = "Write localization Android XML files."
                 setTable(ext.androidTable)
                 if (outputDir == null) {
                     outputDir = project.projectDir.resolve("src/main/resources")
@@ -47,6 +49,7 @@ class LocalePlugin : Plugin<Project> {
             @Suppress("UNUSED_VARIABLE")
             val localizeAll by registering {
                 group = GROUP_NAME
+                description = "Write localization for Java and Android."
                 dependsOn(
                     localizeJava.get(),
                     localizeAndroid.get()
